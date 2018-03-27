37823cb471c2ba66c1d54261d9463c93716c2baf
🦊Tomoya Hirano <noppelabs@gmail.com>
2018-03-27 15:57:22 +0900

    [Swifty]NotificationCenter

--- Pod/Classes/Core/NotificationCenter+TAKExtensions.swift
+++ Pod/Classes/Core/NotificationCenter+TAKExtensions.swift
@@ -9,13 +9,27 @@
 
 import Foundation
 
-public extension NotificationCenter {
-  public func tak_replaceObserver(_ notificationObserver: AnyObject, selector: Selector, identifier: String, object: Any? = nil) {
-    tak_replaceObserver(notificationObserver, selector: selector, name: Notification.Name(rawValue: identifier), object: object)
+extension NotificationCenter: TAKKitCompatible { }
+
+extension TAKKit where Base == NotificationCenter {
+  public func replaceObserver(_ notificationObserver: AnyObject, selector: Selector, identifier: String, object: Any? = nil) {
+    replaceObserver(notificationObserver, selector: selector, name: Notification.Name(rawValue: identifier), object: object)
   }
   
+  public func replaceObserver(_ notificationObserver: AnyObject, selector: Selector, name: Notification.Name, object: Any? = nil) {
+    base.removeObserver(notificationObserver, name: name, object: object)
+    base.addObserver(notificationObserver, selector: selector, name: name, object: object)
+  }
+}
+
+public extension NotificationCenter {
+  @available(*, deprecated, renamed: "tak.replaceObserver(notificationObserver:selector:identifier:object:)")
+  public func tak_replaceObserver(_ notificationObserver: AnyObject, selector: Selector, identifier: String, object: Any? = nil) {
+    tak.replaceObserver(notificationObserver, selector: selector, identifier: identifier, object: object)
+  }
+  
+  @available(*, deprecated, renamed: "tak.replaceObserver(notificationObserver:selector:name:object:)")
   public func tak_replaceObserver(_ notificationObserver: AnyObject, selector: Selector, name: Notification.Name, object: Any? = nil) {
-    removeObserver(notificationObserver, name: name, object: object)
-    addObserver(notificationObserver, selector: selector, name: name, object: object)
+    tak.replaceObserver(notificationObserver, selector: selector, name: name, object: object)
   }
 }
